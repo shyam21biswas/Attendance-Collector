83ae20a2c99695492173a4a2e9de15db5004fa51
shyam21biswas <shyamsundarbiswas21@gmail.com>
2025-03-29 08:18:28 +0530

    final all location case solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+   // implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -64,6 +64,22 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3:1.2.0")
+
+
+
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.activity:activity-compose:1.8.2")
+   // implementation ("androidx.compose.material3:material3:1.2.0")
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    implementation ("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation ("androidx.compose.material3:material3:1.1.0")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.3")
+    implementation ("androidx.compose.ui:ui-tooling:1.5.3")
+
+
 
 
 }
